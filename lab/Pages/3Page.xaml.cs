3c6f18ffa89d37138c3385b800237b3f2e2a7b62
Smerdova Liza <smerdova_vz@mail.ru>
2024-10-12 12:08:59 +0300

    Добавьте файлы проекта.

---
+++ lab/Pages/3Page.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для _3Page.xaml
+    /// </summary>
+    public partial class _3Page : Page
+    {
+        public _3Page()
+        {
+            InitializeComponent();
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.FrameClass.frame.Navigate(new MainPage());
+        }
+
+        private void next_Click(object sender, RoutedEventArgs e)
+        {
+            rez.Text = "";
+            int sizeNumber = Int32.Parse(size.Text);
+            for (int i = 0; i < sizeNumber; i++)
+            {
+                for (int j = 0; j < sizeNumber; j++)
+                {
+                    rez.Text += fillCharacter.Text+" ";
+                }
+                rez.Text += "\n";
+            }
+        }
+
+        private void sizeKV_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(sizeKV.Text))
+            {
+                double sizeK = double.Parse(sizeKV.Text);
+                rez2.Text = Convert.ToString((sizeK * sizeK) / 2);
+            }
+        }
+    }
+}
---
+++ lab/Pages/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.FrameClass.frame.Navigate(new _3Page());
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn10_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn11_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn12_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn13_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn14_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn15_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn16_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
